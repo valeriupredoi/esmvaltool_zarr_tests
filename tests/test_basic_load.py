28d6dd935ee22dea641c366e780f847d3e5547f9
Valeriu Predoi <valeriu.predoi@gmail.com>
2025-08-05 16:21:06 +0100

    add correct numbers

---
+++ tests/test_basic_load.py
@@ -0,0 +1,43 @@
+# import zarr
+# import cf_units
+import iris
+import xarray as xr
+import ncdata
+import ncdata.iris_xarray
+### imports need about 285M
+
+# z = zarr.open('https://uor-aces-o.s3-ext.jc.rl.ac.uk/esmvaltool-zarr/pr_Amon_CNRM-ESM2-1_02Kpd-11_r1i1p2f2_gr_200601-220112.zarr3')
+# ds = z["pr"]
+# chunks = ds.chunks
+# nchunks = ds.nchunks
+# print(chunks)
+# print(nchunks)
+
+
+def load_small_file():
+    zarr_path = (
+        "https://uor-aces-o.s3-ext.jc.rl.ac.uk/"
+        "esmvaltool-zarr/pr_Amon_CNRM-ESM2-1_02Kpd-11_r1i1p2f2_gr_200601-220112.zarr3"
+    )
+
+    time_coder = xr.coders.CFDatetimeCoder(use_cftime=True)
+    zarr_xr = xr.open_dataset(
+        zarr_path,
+        consolidated=True,
+        decode_times=time_coder,
+        engine="zarr",
+        backend_kwargs={},
+    )
+    ### API1: 320MB memory (- 285M = 35MB effective); 1.5s
+
+    conversion_func = ncdata.iris_xarray.cubes_from_xarray
+    cubes = conversion_func(zarr_xr)
+    ### API2: 1GB memory (- 320MB = 680MB effective); 8.5s
+
+    assert isinstance(cubes, iris.cube.CubeList)
+    assert len(cubes) == 1
+    assert cubes[0].has_lazy_data()
+    # d = cubes[0].data
+    # print((cubes[0].data.size / 2**30) * 8)
+
+load_small_file()
